989838ea1e5411f12d2313a75ea338e4c36bb9a8
Gunjan <141393736+GunjanChaudhary1@users.noreply.github.com>
2025-03-04 10:47:45 +0530

    Time: 3 ms (73.59%), Space: 14.7 MB (48.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -13,20 +13,5 @@ public:
             num_map[nums[i]] = i;
         }
         return {};
-        // vector<int> answer;
-        // int sum = 0;
-        
-        // for(int i = 0; i<nums.size(); i++){
-        //     for(int j = i+1; j<nums.size(); j++){
-
-        //         sum = nums[i] + nums[j];
-
-        //         if(sum==target){
-        //             answer.push_back(i);
-        //             answer.push_back(j);
-        //         }
-        //     }
-        // }
-        // return answer; 
     }
 };
