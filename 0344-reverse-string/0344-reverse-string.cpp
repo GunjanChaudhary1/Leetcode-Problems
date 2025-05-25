d7dea98247a0f0f5ae464167bb0cb934b122cd32
Gunjan <141393736+GunjanChaudhary1@users.noreply.github.com>
2025-05-25 14:00:00 +0530

    Time: 0 ms (100%), Space: 27.3 MB (17.75%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
+
+    void reverseStringHelper(vector<char>& s, int left, int right){
+            
+        if (left >= right) return;
+        swap(s[left], s[right]);
+        reverseStringHelper(s, left + 1, right - 1);
+    
+    }
     void reverseString(vector<char>& s) {
+        reverseStringHelper(s, 0, s.size()-1);
 
-        int n=s.size();
-        int left=0;
-        int right=n-1;
+        // int n=s.size();
+        // int left=0;
+        // int right=n-1;
 
-        while(left<right){
-            swap(s[left], s[right]);
-            left++;
-            right--;
-        }
+        // while(left<right){
+        //     swap(s[left], s[right]);
+        //     left++;
+        //     right--;
+        // }
+        
     }
 };
